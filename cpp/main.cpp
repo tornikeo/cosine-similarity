3022630a974bb9403f809ad99c7e885e210dc1bd
tornikeo <tornikeonoprishvili@gmail.com>
2023-11-07 12:33:01 +0200

    Finally! Align cpp with python logic 1-to-1

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -21,7 +21,7 @@ inline Stream& operator<<(Stream& stream, const Vector& vect) {
 	return stream;
 }
 
-template <class E, class E2>
+template <class E, class E2> 
 inline auto trim_values(E&& e, const std::string& direction, E2&& e2)
 {
     XTENSOR_ASSERT_MSG(e.dimension() == 1, "Dimension for trim_zeros has to be 1.");
@@ -58,14 +58,15 @@ const xt::xarray<float> load(string filename) {
 }
 
 int main()
-{
-    const auto all_rmzs = load<float>("references_mz.npy");
-    const auto all_rints = load<float>("references_int.npy");
+{   
+    const auto output_path = "data/results.npy";
+    const auto all_rmzs = load<float>("data/references_mz.npy");
+    const auto all_rints = load<float>("data/references_int.npy");
     // const auto all_rints_bs = load<size_t>("references_batch_size.npy");
 
 
-    const auto all_qmzs = load<float>("queries_mz.npy");
-    const auto all_qints = load<float>("queries_int.npy");
+    const auto all_qmzs = load<float>("data/queries_mz.npy");
+    const auto all_qints = load<float>("data/queries_int.npy");
     // const auto all_qints_bs = load<size_t>("queries_batch_size.npy");
     
     const auto fill_value = static_cast<float>(-1e6);
@@ -129,118 +130,119 @@ int main()
             }
 
 
-            if (matches.empty()) {
-                break;
-            }
+            if (!matches.empty()) {
 
-            // cout << "(" << 
-            //     matches[0].first << "," << 
-            //     matches[0].second << " ), (" <<
-            //     matches[1].first << "," << 
-            //     matches[1].second << ")," << 
-            //     endl;
-            
-            auto idx1 = vector<size_t>();
-            auto idx2 = vector<size_t>();
-
-            for (pair i : matches)
-            {
-                idx1.push_back(i.first);
-                idx2.push_back(i.second);
-            }
-            
-            auto matching_pairs = vector< tuple<size_t, size_t, float> >();
-            
-            for (size_t i = 0; i < idx1.size(); i++)
-            {
-                auto idx = matches[i].first;
-                auto idx2 = matches[i].second;
+                // cout << "(" << 
+                //     matches[0].first << "," << 
+                //     matches[0].second << " ), (" <<
+                //     matches[1].first << "," << 
+                //     matches[1].second << ")," << 
+                //     endl;
                 
-                auto spec1_mz = rmzs(idx);
-                auto spec1_int = rints(idx);
+                auto idx1 = vector<size_t>();
+                auto idx2 = vector<size_t>();
 
-                auto spec2_mz = qmzs(idx2);
-                auto spec2_int = qints(idx2);
-
-                auto power_prod_spec1 = pow(spec1_mz, mz_power) * pow(spec1_int, intensity_power);
-                auto power_prod_spec2 = pow(spec2_mz, mz_power) * pow(spec2_int, intensity_power);
-                // cout << "(" << idx << "," << idx2 << ","
-                //  << power_prod_spec1 << "," << power_prod_spec2 << ")" << endl;
-                // return 0;
-                matching_pairs.push_back(make_tuple(idx, idx2, power_prod_spec1 * power_prod_spec2));
-            }
-
-
-            // cout << "(" << 
-            //     get<0>(matching_pairs[0]) << "," << 
-            //     get<1>(matching_pairs[0]) << "," << 
-            //     get<2>(matching_pairs[0]) << "),(" << 
-            //     get<0>(matching_pairs[1]) << "," << 
-            //     get<1>(matching_pairs[1]) << "," << 
-            //     get<2>(matching_pairs[1]) << ")," << 
-            //     endl;
-            // return 0;
-
-            auto score = 0.0f;
-            auto used_matches = 0ul;
-            auto used1 = std::unordered_set<size_t>();
-            auto used2 = std::unordered_set<size_t>();
-
-            for (size_t i = 0; i < matching_pairs.size(); i++)
-            {
-                auto [idx, idx2, matching_score] = matching_pairs[i];
-                if ( used1.find(idx) == used1.end() && used2.find(idx2) == used2.end() ) {
-                        score += matching_score;
-                        used1.insert(idx);
-                        used2.insert(idx2);
-                        used_matches++;
+                for (pair i : matches)
+                {
+                    idx1.push_back(i.first);
+                    idx2.push_back(i.second);
                 }
+                
+                auto matching_pairs = vector< tuple<size_t, size_t, float> >();
+                
+                for (size_t i = 0; i < idx1.size(); i++)
+                {
+                    auto idx = matches[i].first;
+                    auto idx2 = matches[i].second;
+                    
+                    auto spec1_mz = rmzs(idx);
+                    auto spec1_int = rints(idx);
+
+                    auto spec2_mz = qmzs(idx2);
+                    auto spec2_int = qints(idx2);
+
+                    auto power_prod_spec1 = pow(spec1_mz, mz_power) * pow(spec1_int, intensity_power);
+                    auto power_prod_spec2 = pow(spec2_mz, mz_power) * pow(spec2_int, intensity_power);
+                    // cout << "(" << idx << "," << idx2 << ","
+                    //  << power_prod_spec1 << "," << power_prod_spec2 << ")" << endl;
+                    // return 0;
+                    const auto tmp = make_tuple(idx, idx2, power_prod_spec1 * power_prod_spec2);
+                    matching_pairs.push_back(tmp);
+                }
+
+
+                // cout << "(" << 
+                //     get<0>(matching_pairs[0]) << "," << 
+                //     get<1>(matching_pairs[0]) << "," << 
+                //     get<2>(matching_pairs[0]) << "),(" << 
+                //     get<0>(matching_pairs[1]) << "," << 
+                //     get<1>(matching_pairs[1]) << "," << 
+                //     get<2>(matching_pairs[1]) << ")," << 
+                //     endl;
+                // return 0;
+
+                auto score = 0.0f;
+                auto used_matches = 0ul;
+                auto used1 = std::unordered_set<size_t>();
+                auto used2 = std::unordered_set<size_t>();
+
+                for (size_t i = 0; i < matching_pairs.size(); i++)
+                {
+                    auto [idx, idx2, matching_score] = matching_pairs[i];
+                    if ( used1.find(idx) == used1.end() && used2.find(idx2) == used2.end() ) {
+                            score += matching_score;
+                            used1.insert(idx);
+                            used2.insert(idx2);
+                            used_matches++;
+                    }
+                }
+                // cout << score << endl;
+                // return 0;
+                // cout << score << endl;
+                auto spec1_power = xt::pow(rmzs, mz_power) * xt::pow(rints, intensity_power);
+                auto spec2_power = xt::pow(qmzs, mz_power) * xt::pow(qints, intensity_power);
+                // cout << "Let me guess, the error is here..." << endl;
+                // TODO: Inefficient. Two calls to sum()...
+                // auto x = xt::sum(xt::square(spec1_power), 0);
+                // cout << "NOPE1" << endl;
+                // auto z = xt::square(spec2_power);
+                // cout << "NOPE2" << endl;
+                // auto y = xt::sum(z, 0);
+                // cout << "NOPE3" << endl;
+                // auto score_norm = sqrt(x * y);
+
+                auto score_norm = sqrt(xt::sum(xt::square(spec1_power)) * xt::sum(xt::square(spec2_power)));
+                // cout << std::vector<double>(spec1_power.begin(), spec1_power.end()) << endl;
+                
+                // cout << std::vector<double>(spec2_power.begin(), spec2_power.end()) << endl;
+
+                // cout << score << "/" << score_norm << endl;
+                // return 0;
+                
+                // cout << "(" << spec1_power[0] << ","  
+                //             << spec1_power[1] << "," 
+                //             << spec1_power[2] << "," 
+                //             << spec1_power[3] << "," 
+                //             << spec1_power[4] << "...),(" 
+                //             << spec2_power[0] << "," 
+                //             << spec2_power[1] << "," 
+                //             << spec2_power[2] << "," 
+                //             << spec2_power[3] << "," 
+                //             << spec2_power[4] << "...)," << 
+                //     endl;
+                // return 0;
+                // auto score_norm = xt::sum(spec1_power) * xt::sum(spec2_power);
+                // auto score_norm = 0.0f;
+                // for(auto it=spec1_power.begin(); it!=spec1_power.end(); ++it)
+                // {
+                //     score_norm += *it;
+                // }
+                
+                // auto score_norm = xt::sum(spec1_power) * xt::sum(spec2_power);
+                auto score_norm_result = score_norm();
+                score = score / score_norm_result;
+                results.insert(results.end(), {score, static_cast<double>(used_matches)});
             }
-            // cout << score << endl;
-            // return 0;
-            // cout << score << endl;
-            auto spec1_power = xt::pow(rmzs, mz_power) * xt::pow(rints, intensity_power);
-            auto spec2_power = xt::pow(qmzs, mz_power) * xt::pow(qints, intensity_power);
-            // cout << "Let me guess, the error is here..." << endl;
-            // TODO: Inefficient. Two calls to sum()...
-            // auto x = xt::sum(xt::square(spec1_power), 0);
-            // cout << "NOPE1" << endl;
-            // auto z = xt::square(spec2_power);
-            // cout << "NOPE2" << endl;
-            // auto y = xt::sum(z, 0);
-            // cout << "NOPE3" << endl;
-            // auto score_norm = sqrt(x * y);
-
-            auto score_norm = sqrt(xt::sum(xt::square(spec1_power)) * xt::sum(xt::square(spec2_power)));
-            // cout << std::vector<double>(spec1_power.begin(), spec1_power.end()) << endl;
-            
-            // cout << std::vector<double>(spec2_power.begin(), spec2_power.end()) << endl;
-
-            // cout << score << "/" << score_norm << endl;
-            // return 0;
-            
-            // cout << "(" << spec1_power[0] << ","  
-            //             << spec1_power[1] << "," 
-            //             << spec1_power[2] << "," 
-            //             << spec1_power[3] << "," 
-            //             << spec1_power[4] << "...),(" 
-            //             << spec2_power[0] << "," 
-            //             << spec2_power[1] << "," 
-            //             << spec2_power[2] << "," 
-            //             << spec2_power[3] << "," 
-            //             << spec2_power[4] << "...)," << 
-            //     endl;
-            // return 0;
-            // auto score_norm = xt::sum(spec1_power) * xt::sum(spec2_power);
-            // auto score_norm = 0.0f;
-            // for(auto it=spec1_power.begin(); it!=spec1_power.end(); ++it)
-            // {
-            //     score_norm += *it;
-            // }
-            
-            // auto score_norm = xt::sum(spec1_power) * xt::sum(spec2_power);
-            score = score / score_norm();
-            results.insert(results.end(), {score, static_cast<double>(used_matches)});
         }
     }
     
@@ -264,11 +266,9 @@ int main()
     //     }
     // }
 
-    
-
     cout << "Dumping results.npy..." << endl;
     auto xresults = xt::adapt(results, {results.size()/2, 2ul});
-    xt::dump_npy("results.npy", xresults);
+    xt::dump_npy(output_path, xresults);
     
     cout << "Done!" << endl;
     // xt::xarray<float> a = {{1,2,3,4}, {5,6,7,8}};
