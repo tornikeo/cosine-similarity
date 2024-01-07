f883dd8e5f2bceceb2c7b66d39ae43830cbd367d
tornikeo <tornikeonoprishvili@gmail.com>
2024-01-07 21:04:55 +0400

    Make sorting stable so cosine similarity is 100% repeatable

--- cudams/kernel.py
+++ cudams/kernel.py
@@ -1,7 +1,6 @@
 import math
 import numba
 from numba import cuda, types
-from numba.cuda.cudadrv.devicearray import DeviceNDArray
 
 
 def compile(
@@ -19,11 +18,11 @@ def compile(
             DeviceNDArray, [2, R, M] float32
         qspec_cu:
             DeviceNDArray, [2, Q, N] float32
-        lens_cu: DeviceNDArray, [2, max(R,Q)] int32
+        lens_cu, [2, max(R,Q)] int32
             The "2" in front is because these is mz and int stacked on top of each other
-        out_cu: DeviceNDArray, [R,Q,2] float32
+        out_cu, [R,Q,2] float32
             Contains both score (0) and counts (1) for each RQ pair
-        overflow_cu: DeviceNDArray, [R,Q,1] uint8
+        overflow_cu, [R,Q,1] uint8
             Contains 1 - if overflow happened at RQ
         stream: cuda.stream
             Necessary to keep GPU as busy as possible.
@@ -40,11 +39,11 @@ def compile(
 
     @cuda.jit
     def _kernel(
-        rspec: DeviceNDArray,
-        qspec: DeviceNDArray,
-        lens: DeviceNDArray,
-        out: DeviceNDArray,
-        overflow: DeviceNDArray,
+        rspec,
+        qspec,
+        lens,
+        out,
+        overflow,
     ):
         i, j = cuda.grid(2)
         thread_i = cuda.threadIdx.x
@@ -89,7 +88,6 @@ def compile(
             matches = cuda.local.array((2, MATCH_LIMIT), types.int16)
             for peak1_idx in range(rleni):
                 mz = spec1_mz[peak1_idx]
-
                 low_bound = mz - tolerance
                 high_bound = mz + tolerance
 
@@ -98,7 +96,7 @@ def compile(
                     if mz2 > high_bound:
                         break
                     if mz2 < low_bound:
-                        lowest_idx = peak2_idx
+                        lowest_idx = peak2_idx + 1
                     else:
                         if num_match < MATCH_LIMIT:
                             matches[0, num_match] = peak1_idx
@@ -107,16 +105,18 @@ def compile(
                         else:
                             overflow[i, j, 0] = 1  # This is the errorcode for overflow
                             break
-
+            # out[i, j, 0] = 1
+            # out[i, j, 1] = num_match
+            # return
             if num_match == 0:
                 return
-
-            # SLOW, calculate norm ( This should be done in several threads )
-            # score_norm = types.float32(0.0)
+            
+            
             score_norm = types.float64(1.0)
             score_norm_spec1 = types.float64(0.0)
             score_norm_spec2 = types.float64(0.0)
 
+            # TODO: It is faster to pre-compute this for every R,Q beforehand and pass it in.
             for peak1_idx in range(rleni):
                 score_norm_spec1 += (
                     (spec1_mz[peak1_idx] ** mz_power)
@@ -135,11 +135,11 @@ def compile(
             used_matches = types.int32(0)
             for _ in range(0, num_match):
                 max_prod = types.float64(-1.0)
-                max_peak1_idx = -1
-                max_peak2_idx = -1
+                # max_peak1_idx = types.int16(-1)
+                # max_peak2_idx = types.int16(-1)
 
                 for sj in range(0, num_match):
-                    if matches[0, sj] >= 0:
+                    if matches[0, sj] != -1:
                         peak1_idx = matches[0, sj]
                         peak2_idx = matches[1, sj]
 
@@ -150,12 +150,13 @@ def compile(
                             spec2_int[peak2_idx] ** int_power
                         )
                         prod = power_prod_spec1 * power_prod_spec2
-                        if prod > max_prod:
+
+                        if prod >= max_prod:
                             max_prod = prod
                             max_peak1_idx = peak1_idx
                             max_peak2_idx = peak2_idx
 
-                if max_prod > 0:
+                if max_prod != -1:
                     for sj in range(0, num_match):
                         if (
                             matches[0, sj] == max_peak1_idx
@@ -165,8 +166,7 @@ def compile(
                             matches[1, sj] = -1  # "Remove" it
                     score += max_prod
                     used_matches += 1
-
-                if max_prod < 0:
+                else:
                     break
 
             score = score / score_norm
@@ -175,11 +175,11 @@ def compile(
             out[i, j, 1] = used_matches
 
     def kernel(
-        rspec_cu: DeviceNDArray,
-        qspec_cu: DeviceNDArray,
-        lens_cu: DeviceNDArray,
-        out_cu: DeviceNDArray,
-        overflow_cu: DeviceNDArray,
+        rspec_cu,
+        qspec_cu,
+        lens_cu,
+        out_cu,
+        overflow_cu,
         stream: cuda.stream = None,
     ):
         _kernel[BLOCKS_PER_GRID, THREADS_PER_BLOCK, stream](
