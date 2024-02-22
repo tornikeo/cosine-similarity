0f4929e17acc505067178a02aa9f4013cddb12c5
tornikeo <tornikeonoprishvili@gmail.com>
2024-02-22 17:08:54 +0200

    Make gh workflow use cuda sim instead

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,14 @@
 import pytest, warnings
 
 @pytest.fixture(autouse=True, scope='session')
-def use_cudasim_if_cuda_unavailable():
+def warn_on_no_cuda():
     import os, numba
-    
-    os.environ['NUMBA_DISABLE_PERFORMANCE_WARNINGS'] = '1'
-    
-    has_cuda = numba.cuda.is_available()
-    
-    if not has_cuda:
-        warnings.warn("CUDA was unavailable - using numba cuda simulator for testing")
-        os.environ['NUMBA_ENABLE_CUDASIM'] = '1'
-
+    if not numba.cuda.is_available():
+        warnings.warn("CUDA was unavailable - consider using `NUMBA_ENABLE_CUDASIM=1 pytest <same args, if any>` to simulate having GPU and cudatoolkit for testing purposes")
     yield
-    
-    if not has_cuda:    
-        del os.environ['NUMBA_ENABLE_CUDASIM']
-        del os.environ['NUMBA_DISABLE_PERFORMANCE_WARNINGS']
+        
+@pytest.fixture(autouse=True, scope='session')
+def ignore_warnings():
+    import os
+    os.environ['NUMBA_DISABLE_PERFORMANCE_WARNINGS'] = '1'
+    yield
